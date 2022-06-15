e78961e4b3484e211ee6fb50ad919cf23e7f98b3
emarkees <emarkees@gmail.com>
2022-06-15 10:22:33 +0100

    Dryer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchboxComponent } from './searchbox/searchbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchboxComponent
   ],
   imports: [
     BrowserModule,
